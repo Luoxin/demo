38919a878b98cc81e29ea1589cca89ae1a3947ea
落心 <luoxin.ttt@gmail.com>
2023-05-15 22:10:27 +0800

    嵌入前端内容

---
+++ src/GetCookieData.js
@@ -0,0 +1,27 @@
+import cookie from "react-cookies";
+
+export default function GetCookieData() {
+    /*let cookieData = {
+        token: "",
+        info: "",
+    }
+    let cookies = document.cookie
+    let cookieArr = cookies.split(';')
+    for(let i = 0; i < cookieArr.length; i++) {
+        let arr = cookieArr[i].split('=')
+        if (" token" === arr[0] ) {
+            cookieData.token = arr[1]
+        }
+        if ("info" === arr[0] ) {
+            cookieData.info = JSON.parse(unescape(decodeURI(arr[1])))
+        }
+    }*/
+    const cookieData = {
+        token: cookie.load('token'),
+        info: cookie.load('info'),
+    }
+
+    console.log(cookieData)
+
+    return cookieData
+}
